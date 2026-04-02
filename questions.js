801ce09342cab155c9c83513f385e4e9813b3fd4
Dimpoltravel <dimpol.bailung@gmail.com>
2026-04-02 10:35:04 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,29 @@
+const tests = [
+  {
+    name:"ADRE Mock Test",
+    time:60,
+    questions:[
+      {
+        q:"Capital of Assam?",
+        a:["Guwahati","Dispur","Jorhat","Dibrugarh"],
+        correct:1
+      },
+      {
+        q:"2 + 2 = ?",
+        a:["3","4","5","6"],
+        correct:1
+      }
+    ]
+  },
+  {
+    name:"SSC Practice Test",
+    time:30,
+    questions:[
+      {
+        q:"India Independence year?",
+        a:["1945","1947","1950","1930"],
+        correct:1
+      }
+    ]
+  }
+];
